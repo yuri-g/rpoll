2aa72970e08719c82d9d67dc87f1a61ceaf4937c
yuri-g <yuri.goncharenko@gmail.com>
2014-04-28 12:52:35 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rpoll::Application.config.secret_key_base = '4466bf5bf22b74344ea9e5441d70aaec76b3864fec7e10777966b91afcda71027caa523b99dc184701cf86d447711ef01ad53d36b647a5a8ae7385c45e5818e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rpoll::Application.config.session_store :cookie_store, key: '_rpoll_session'
